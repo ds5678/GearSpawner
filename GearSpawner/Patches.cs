517a3daa298796cb98020eb77e66098b3b3609a3
Jeremy Pritts <jrpritts7@gmail.com>
2022-07-29 12:14:24 -0400

    formatting in patches

--- GearSpawner/Patches.cs
+++ GearSpawner/Patches.cs
@@ -6,7 +6,7 @@ namespace GearSpawner;
 
 internal static class Patches
 {
-	[HarmonyPatch(typeof(LootTable), "GetPrefab")]
+	[HarmonyPatch(typeof(LootTable), nameof(LootTable.GetPrefab))]
 	internal static class LootTable_GetPrefab
 	{
 		private static void Prefix(LootTable __instance)
@@ -15,7 +15,7 @@ internal static class Patches
 		}
 	}
 	
-	[HarmonyPatch(typeof(LootTable), "GetRandomGearPrefab")]
+	[HarmonyPatch(typeof(LootTable), nameof(LootTable.GetRandomGearPrefab))]
 	internal static class LootTable_GetRandomGearPrefab
 	{
 		private static void Prefix(LootTable __instance)
@@ -24,7 +24,7 @@ internal static class Patches
 		}
 	}
 
-	[HarmonyPatch(typeof(GameManager), "SetAudioModeForLoadedScene")]//Exists
+	[HarmonyPatch(typeof(GameManager), nameof(GameManager.SetAudioModeForLoadedScene))]//Exists
 	internal static class GameManager_SetAudioModeForLoadedScene
 	{
 		private static void Prefix()
